788f818c0df538ed478720c5df273c46bb433675
AndrewL665 <aleong@umass.edu>
2025-03-20 18:36:30 -0400

    Added pydantic
    Added pydantic to structure notifications and added class for each notification type.

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,5 +1,6 @@
 import os
 from pymongo import MongoClient
+from pydantic import BaseModel
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,6 +16,37 @@ def insert_notification_test(user_id):
     }
     collection.insert_one(notification)
     return "Notification added successfully"
+
+class PolicyNotification(BaseModel):
+    policy_id:str
+    userId:str
+    subject:str
+    body:str
+    is_Read:bool = False
+    is_Archived:bool = False
+
+class NewsNotification(BaseModel):
+    userId:str
+    is_Read:bool = False
+    created_Date:str
+    expiration_Date:str
+    type:str
+    title:str
+    details:str
+
+class ClaimsNotification(BaseModel):
+    insured_Name:str
+    claimant_Name:str
+    task_Type:str
+    username:str
+    due_Date:str
+    line_Business:str
+    description:str
+    priority:str
+    is_Completed:bool = False
+
+
+
 
 def get_notifications_test():
     return list(collection.find({}, {"_id": 0}))
