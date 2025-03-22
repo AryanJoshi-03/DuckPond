0373c3fe0613bc6103a894eb4496311da61073ae
AndrewL665 <aleong@umass.edu>
2025-03-22 15:54:19 -0400

    Added a GET, POST HTTP methods
    Added a way to get a notification depending on user_id and to add a notification

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,7 +1,9 @@
 import os
+from typing import List
 from pymongo import MongoClient
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
 
 load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
@@ -9,6 +11,8 @@ MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["notifications"]
 collection = db["notifications"]
+
+app = FastAPI()
 
 def insert_notification_test(user_id):
     notification = { 
@@ -46,7 +50,32 @@ class ClaimsNotification(BaseModel):
     is_Completed:bool = False
 
 
-
-
-def get_notifications_test():
+#Get all notifications
+@app.get("/notifications", response_model=List[dict])
+def get_all_notifications():
     return list(collection.find({}, {"_id": 0}))
+
+#Get notification depending on user_id **CAN CHANGE**
+@app.get("/notifications/user/{user_id}",response_model=dict)
+def get_notification_user(user_id:str):
+    user_notif = list(collection.find({"user_Id":user_id}, {"_id":0}))
+    if not user_notif:
+        raise HTTPException()
+    return user_notif
+
+#Create a notification depending on type
+@app.post("/notifications/{notification_type}")
+def create_notification(notification_type:str, notification_data:dict):
+    if notification_type not in ["policy", "news","claims"]:
+        raise HTTPException()
+
+    if notification_type == "policy":
+        notification = PolicyNotification(**notification_data)
+    elif notification_type == "claims":
+        notification = ClaimsNotification(**notification_data)
+    elif notification_type == "news":
+        notification = NewsNotification(**notification_data)
+
+    notification_dict = notification.model_dump()
+    notification_dict["type"] = notification_type
+    collection.insert_one(notification_dict)
